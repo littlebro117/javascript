9ff9d5304959b5b2410e7ea79f0bcd20697655f6
littlebro117 <qwertyheadphones064o@gmail.com>
2024-12-14 11:59:22 +0530

    first commit

---
+++ bom.js
@@ -0,0 +1,68 @@
+function okk1()
+{
+let pr=prompt("enter the price")
+let cop=confirm("and do u have copens")
+if(cop==true){
+pr=(pr)-(pr/10);
+alert(pr)
+}
+
+}
+
+
+
+
+okk1();
+
+// function okk2(){
+// let pr=prompt("enter your exam score")
+// if(pr>33)
+//     alert("conj u did it")
+// else
+// alert('fail')
+
+// }
+// okk2();
+
+
+// function okk3(){
+
+//  let cop=confirm("are you sure to delete")
+
+//  if(cop==1)
+// alert("your item is deleted")
+//  else
+//  alert("cancelled")
+
+// }
+//     okk3();
+
+
+//     function okk4(){
+
+// let a= prompt("enter a number")
+// if(a>0)
+// alert("its a positive")
+
+
+
+//     else if(a==0)
+// alert("zero")
+//     else
+//     alert("ita a negative numer")
+//     }
+//         okk4();
+
+//   function okk5(){
+
+//  let i=1;
+//     while(i <=10 ){
+       
+// console.log(i);
+      
+//         }
+//         i++;
+     
+    
+//   }
+//   okk5();
---
+++ bom2.js
@@ -0,0 +1,69 @@
+//   function okk5(){
+
+//  let i=1;
+//     while(i <= 10 ){
+       
+// console.log(i);
+//       i++;
+//         }
+        
+     
+    
+//   }
+//   okk5();
+
+//   function okk5(){
+//     let num;
+//     do{
+// num = parseInt(window.prompt("enter a number greater then ten"))
+                                     
+//     }while(num<=10)
+// }
+//     okk5();
+
+    //   function okk6(){
+
+//        let week =parseInt( prompt("enter number 1-7"))
+// switch (week) {
+//     case 1: alert("monday")
+//      break;
+//     case 2: alert("tuesday")
+//      break;
+//     case 3: alert("wednsday")
+//      break;
+
+//     case 4: alert("thursday")
+//      break;
+//     case 5: alert("friday")
+//      break;
+
+//     case 6: alert("saturday")
+//      break;
+
+//     case 7: alert("sunday")
+//      break;
+
+//    default:alert("enter a number")
+   
+// }
+
+//       }
+//           okk6();
+let green = () => {
+
+   let show= document.querySelector("#box")
+    show.style.backgroundColor="green";
+    // document. body. style. backgroundColor="green";
+
+
+}
+let red =()=>{
+    let show2=document.querySelector("#box")
+    // show2. innerHTML="r"
+    show2. style. backgroundColor="red";
+}
+
+// let blue=()=>{
+// let show3=document.querySelector("#box")
+// show3. style. backgroundColor="blue";
+// }
