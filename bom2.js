4ff6796f8abbdd1fee62df9c2584fd346f390f73
littlebro117 <qwertyheadphones064o@gmail.com>
2025-01-04 19:06:42 +0530

    Your commit message

--- bom2.js
+++ bom2.js
@@ -49,21 +49,146 @@
 
 //       }
 //           okk6();
-let green = () => {
+// let green = () => {
 
-   let show= document.querySelector("#box")
-    show.style.backgroundColor="green";
-    // document. body. style. backgroundColor="green";
+//    let show= document.querySelector("#box")
+//     show.style.backgroundColor="green";
+//     // document. body. style. backgroundColor="green";
 
 
-}
-let red =()=>{
-    let show2=document.querySelector("#box")
-    // show2. innerHTML="r"
-    show2. style. backgroundColor="red";
-}
+// }
+// let red =()=>{
+//     let show2=document.querySelector("#box")
+//     // show2. innerHTML="r"
+//     show2. style. backgroundColor="red";
+// }
 
 // let blue=()=>{
 // let show3=document.querySelector("#box")
 // show3. style. backgroundColor="blue";
 // }
+// let name=window.prompt("whats your name")
+// alert(`"hellow" ${name}`)
+
+//  let like=confirm("do u like javascript")
+
+//  if(like==1){
+// alert("ok")
+//  } else{
+// alert("its ok")
+
+//  }
+// let show =()=>{
+// let str =(`"javascript is easy. Ilove javascript!"`)
+// document.write(str.replace(/javascript/ig,"coding"))
+
+// }
+// show();
+                                // Question4
+// for(let  i =1;i<11;++i){
+//     console.log(i)
+// }
+
+                                            // question5
+//     let arr=["apple","mango","papya"]
+//   for(let i=3;i>0;--i)   {                                     
+// let ok =prompt("what are your 3 fav fruits")
+
+//  document.write(arr.push("orange"))
+//   }
+// ans2)
+  
+//     let fruits =[]
+//      for(let i=3;i>0;--i)   { 
+//     let fav=prompt("ewhat are your fav frutits")
+    
+//      fruits.push(fav)}
+//    // add onlt last eliments from the array from the last for thr array
+//    console.log(fruits)
+
+                                            //   question7
+
+//     let arr=[10,20,30,40,50]   
+//   let show=0;
+// for(let i=0;i<5;++i)
+//     {
+//  show=show+arr[i];
+
+// }
+//   document.write(show)
+
+
+
+                                          // question8
+//   let go=()=>{
+
+// let arr=[]
+
+//       let num=prompt("enter 5 numbers")                                       
+//       let  
+//  }  
+// let arr=[2,5,9,7,3,1,8,6]
+//   let go = a.filter((num)=>num>3)//num%2!==0 for odd number//filter will work on every element and fillter out some sepsfie elements
+// document.write(ans)
+
+// let ans1 = arr.map(function(num){
+//     return num*2
+// })
+
+// console.log(ans1)
+// document.write(ans1)
+
+// let ans2 = arr.map((num)=>{
+// return num+num/2
+// })
+// document.write(ans2)
+
+// let anst=arr.filter(function okk(num){
+//    return num<3})
+// document.write(arr) 
+
+                                          // question8
+// let arr = []
+// for(let i=1;i<6;++i){
+// let num= parseInt( prompt("enter five numbers "))
+
+// arr.push(num)
+// }
+// // console.log(num)
+
+// let ans =arr.filter((num)=>num%2==0)
+
+
+// alert(ans)
+                                          // question9
+// let str = ('html is easy . html is fun')
+
+// let t=str.replace(/html/ig , "javascript")
+// //  console.log(t)
+// let y = t.toUpperCase()
+// alert(y)
+                                       //  question10
+                                
+                                       
+//  let num = parseInt(prompt("enter a number bettven 1-10"))
+//  for(let i=1;i<11;++i)
+//    console.log(num*i)
+
+                                    //  practice questions
+                        //  
+//     let arr=[]
+//     for(let i=0;i<5;++i){
+//  let val=parseInt(prompt("enter any five numbers"))
+//  arr.push(val);
+//     }
+//     // arr.filter((num)=>num%2==0)
+//          let ans= arr.filter((num)=>{
+//      return   num%2==0})
+//      console.log(ans)
+// let num=prompt("enter a number")
+//                    for(let i=1;i<11;i++){
+//                            let m = i*num;
+                        
+//                         console.log(m)
+//                     }
+//                     Arr.map((num)=>{})
